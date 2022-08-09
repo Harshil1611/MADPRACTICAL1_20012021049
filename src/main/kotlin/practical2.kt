feb322507d8f7d0665af18bc563dc24cdde0a79a
Karan Sethia <karansethia20@gnu.ac.in>
2022-08-09 12:22:04 +0530

    practical2.kt  is of data types conversion printing on screen

---
+++ src/main/kotlin/practical2.kt
@@ -0,0 +1,13 @@
+fun main(){
+    println("Enter an Integer")
+    val a= readLine()!!.toDouble()
+    println("your double $a")
+
+    println("Enter an String")
+    val b= readLine()!!.toInt()
+    println("your double $b")
+
+    println("Enter an String")
+    val c= readLine()!!.toDouble()
+    println("your double $c")
+}
