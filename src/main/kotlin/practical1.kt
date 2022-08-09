7a9779a73658e0bad606322b908618a6c26a5fdf
Karan Sethia <karansethia20@gnu.ac.in>
2022-08-09 12:05:23 +0530

    practical1.kt  is of data types printing on screen

---
+++ src/main/kotlin/practical1.kt
@@ -0,0 +1,23 @@
+import java.time.Year
+
+fun main(){
+    val a: Int = 100
+    print("this is your int $a \n")
+    val b = 1.5
+    print("this is your float $b\n")
+    val char ='Y'
+    print("this is your Char $char\n")
+    val str= "harshil"
+    print("this is your String $str\n")
+    val bool =true
+    print("this is your Bool $bool\n")
+    val double=96.36
+    print("this is your Doubele $double\n")
+    val long=  854645758
+    print("this is your Long $long\n")
+    val shrt =-5263
+    print("this is your Short $shrt\n")
+    val byt= 215
+    print("this is your Byte $byt\n")
+
+}
