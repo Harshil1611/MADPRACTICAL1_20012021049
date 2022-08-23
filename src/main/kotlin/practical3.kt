2735d0cd4e48473deb0198390fb06dd7787e5862
Harshil Raval <ravalharshil1625@gmail.com>
2022-08-23 15:18:01 +0100

    Practical 2.kt

---
+++ src/main/kotlin/practical3.kt
@@ -0,0 +1,37 @@
+fun main(){
+    print("Enter your name")
+    val name = readLine().toString()
+
+    println("Enter your Enrollment number ")
+    val en = readLine().toString()
+
+    println("Your branch Branch")
+    val branch = readLine().toString()
+
+    println("Your class")
+    val clas = readLine().toString()
+
+    println("Batch")
+    val batch = readLine().toString()
+
+    println("Enter your college name")
+    val clgname= readLine().toString()
+
+    println("Enter your University name")
+    val uniname = readLine().toString()
+
+    println("Enter Age")
+    val age = readLine().toString()
+
+    print("Student's name- $name")
+    println("Student's Enrollment number- $en")
+    println("Student's Branch $branch")
+    println("Student's class $clas")
+    println("Student's Batch $batch")
+    println("Student's college name $clgname")
+    println("Student's University name $uniname")
+    println("Student's Age $age")
+
+
+
+}
